4ebb7a0c94f2b50ea85932317290ecd5c4724404
Nguyen Van Thinh <nguyenvanthinh0817@gmail.com>
2022-04-16 12:09:16 +0700

    update

--- Base/bc/Player.js
+++ Base/bc/Player.js
@@ -1,65 +1,62 @@
 module.exports = class Player {
-  id = null;
-  username = null;
-  cards = [];
-  highestCard = null;
-  betAmount = 0;
-  amonunt = 0;
-  constructor(userId, username) {
-    this.id = userId;
-    this.username = username;
-  }
-
-  addCard(card) {
-    this.cards.push(card);
-    this.findHighestCard(card);
-  }
-
-  findHighestCard(card) {
-    if (!this.highestCard) {
-      this.highestCard = card;
-    } else {
-      this.highestCard =
-        card.cardNumber + card.typeCard >
-        this.highestCard.cardNumber + this.highestCard.typeCard
-          ? card
-          : this.highestCard;
+    id = null
+    username = null
+    cards = []
+    highestCard = null
+    betAmount = 0
+    amonunt = 0
+    constructor(userId, username) {
+        this.id = userId
+        this.username = username
     }
-  }
 
-  checkExistCard(card) {
-    const newCard = this.cards(
-      (item) =>
-        item.cardNumber === card.cardNumber && item.cardType === card.cardType
-    );
-
-    if (newCard) {
-      return true;
-    } else {
-      return false;
+    addCard(card) {
+        this.cards.push(card)
+        this.findHighestCard(card)
     }
-  }
 
-  getPoint() {
-    const totalPoint = this.cards.reduce(
-      (totalPoint, item) => totalPoint + item.cardNumber,
-      0
-    );
+    findHighestCard(card) {
+        if (!this.highestCard) {
+            this.highestCard = card
+        } else {
+            this.highestCard =
+                card.cardNumber + card.typeCard >
+                this.highestCard.cardNumber + this.highestCard.typeCard
+                    ? card
+                    : this.highestCard
+        }
+    }
 
-    return totalPoint % 10 == 0 ? 10 : totalPoint % 10;
-  }
+    checkExistCard(card) {
+        const newCard = this.cards(
+            (item) =>
+                item.cardNumber === card.cardNumber &&
+                item.cardType === card.cardType
+        )
 
-  getTypePoint() {
-    return this.cards.reduce(
-      (totalTypePoint, item) =>
-        totalTypePoint + item.cardNumber === 1
-          ? item.cardNumber * 10
-          : item.cardNumber,
-      0
-    );
-  }
+        if (newCard) {
+            return true
+        } else {
+            return false
+        }
+    }
 
-  getCards() {
-    return this.cards.map((card) => card.mapCard());
-  }
-};
+    getPoint() {
+        const totalPoint = this.cards.reduce(
+            (totalPoint, item) => totalPoint + item.cardNumber,
+            0
+        )
+
+        return totalPoint % 10 == 0 ? 10 : totalPoint % 10
+    }
+
+    getTypePoint() {
+        return this.highestCard.cardNumber === 1
+            ? this.highestCard.typeCard + 10
+            : this.highestCard.typeCard + this.highestCard.cardNumber
+    }
+
+    getCards() {
+        return this.cards.map((card) => card.mapCard())
+    }
+}
