66f89e49383f03f456dc4fa8b27fdc50ce3c38d3
Nguyen Danh Da <nguyendanhda98@gmail.com>
2022-04-17 22:06:23 +0700

    update

---
+++ commands/3cayBot.js
@@ -0,0 +1,121 @@
+const messageEmbed = require('../util/messageEmbed');
+const GameTable = require('../Base/bc/GameTable');
+const GameTableManagement = require('../Base/bc/GameTableManagement');
+const { updateUser } = require('../services/profile');
+const gameTableManagement = GameTableManagement.getInstance();
+const dndEmoji = process.env.DND_EMOJI;
+module.exports = {
+  name: '3caybot',
+  aliases: ['3cb'],
+  cooldown: 10,
+  permissions: [],
+  description: 'choi Ba Cay',
+
+  async execute(message, args, cmd, client, discord, profileData) {
+    let response;
+    var extra = {};
+
+    const amount = args[0];
+    //kiem tra DND
+    if (amount % 1 != 0 || amount <= 0) {
+      extra = {
+        setDescription: 'Số DND không hợp lệ!',
+      };
+    } else if (amount > profileData.cash) {
+      extra = {
+        setDescription: 'Bạn không có đủ DND trong ví',
+      };
+    } else {
+      const host = { id: 'bot', username: 'bot' };
+      const user1 = {
+        id: message.author.id,
+        username: message.author.username,
+      };
+
+      const game = new GameTable(host, 2);
+
+      game.join(user1);
+      game.ready(host);
+      game.bet(user1, +amount);
+      game.distributeCards(host);
+      const {
+        winners,
+        playersInfo,
+        message: checkWinnersMessage,
+      } = game.checkWinners(host);
+
+      // extra = {
+      //     setDescription: `Người chiến thắng là: ...`,
+      //     addFields: playersInfo.map((player) => ({
+      //         name: player.username,
+      //         value: `${player.getShowCards(0).join(' ')}`,
+      //         inline: true,
+      //     })),
+      // }
+
+      extra = {
+        setDescription: `Người chiến thắng là: ...`,
+        addFields: [
+          {
+            name: playersInfo[1].username,
+            value: `${playersInfo[1].getShowCards(0).join(' ')}`,
+            inline: true,
+          },
+          {
+            name: playersInfo[0].username,
+            value: `${playersInfo[0].cards.join(' ')}`,
+            inline: true,
+          },
+        ],
+      };
+
+      const getEditEmbed = (numberCard, showAmount = false) => {
+        extra.addFields = [
+          {
+            name: `${playersInfo[1].username}${
+              showAmount ? `\n${playersInfo[1].showAmount}` : ''
+            }`,
+
+            value: `${playersInfo[1].getShowCards(numberCard).join(' ')}`,
+            inline: true,
+          },
+          {
+            name: `${playersInfo[0].username}${
+              showAmount ? `\n${playersInfo[0].showAmount}` : ''
+            }`,
+            value: `${playersInfo[0].cards.join(' ')}`,
+            inline: true,
+          },
+        ];
+        response.edit({
+          embeds: [messageEmbed(message, discord, extra)],
+        });
+      };
+
+      setTimeout(() => {
+        getEditEmbed(1);
+        setTimeout(() => {
+          getEditEmbed(2);
+          setTimeout(() => {
+            getEditEmbed(3);
+            setTimeout(() => {
+              extra.setDescription = `Người chiến thắng là: ${checkWinnersMessage}`;
+              getEditEmbed(3, true);
+            }, 2000);
+          }, 3000);
+        }, 2000);
+      }, 1000);
+
+      if (winners.length === 0) {
+        await updateUser(message.author.id, {
+          cash: +amount * -1,
+        });
+      } else {
+        await updateUser(message.author.id, { cash: +amount });
+      }
+    }
+
+    const newEmbed = messageEmbed(message, discord, extra);
+    response = await message.channel.send({ embeds: [newEmbed] });
+  },
+};
--- commands/help.js
+++ commands/help.js
@@ -19,7 +19,7 @@ module.exports = {
         { name: 'de', value: `Chơi đề. \`de <tiền cược> <số cược>\`` },
         { name: 'money', value: 'Kiểm tra số tiền của mình' },
         { name: 'add-money', value: 'Cộng tiền cho người chơi' },
-        { name: '3cay', value: `Chơi 3 cây. \`3cay <tiền cược>\`` },
+        { name: '3caybot', value: `Chơi 3 cây. \`3caybot <tiền cược>\`` },
         {
           name: 'Tạo bàn chơi 3 cây',
           value: `\`3cay new\` \`3cay join @user\` \`3cay ready\` \`3cay bet <tiền cược>\` \`3cay start\` \`3cay info\``,
