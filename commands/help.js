43aeb48ca28067337e905acd55cea36e95f83fa8
Nguyen Danh Da <nguyendanhda98@gmail.com>
2022-04-17 14:20:23 +0700

    help 3cay

--- commands/help.js
+++ commands/help.js
@@ -1,25 +1,29 @@
-const messageEmbed = require("../util/messageEmbed");
+const messageEmbed = require('../util/messageEmbed');
 
 module.exports = {
-  name: "help",
+  name: 'help',
   aliases: [],
   cooldown: 0,
   permissions: [],
-  description: "this is help command",
+  description: 'this is help command',
   execute(message, args, cmd, client, discord, profileData) {
     const extra = {
-      setTitle: "Hướng dẫn sử dụng",
+      setTitle: 'Hướng dẫn sử dụng',
       setDescription: `Prefix: \`d.\``,
       addFields: [
-        { name: "work", value: "Làm việc kiếm tiền" },
-        { name: "with", value: "Rút tiền từ ngần hàng" },
-        { name: "dep", value: "Cất tiền vào ngân hàng" },
-        { name: "cf", value: `Đá gà. \`cf <tiền cược>\`` },
-        { name: "give", value: `Chuyển tiền. \`give <người nhận> <số tiền>\`` },
-        { name: "de", value: `Chơi đề. \`de <tiền cược> <số cược>\`` },
-        { name: "money", value: "Kiểm tra số tiền của mình" },
-        { name: "add-money", value: "Cộng tiền cho người chơi" },
-        { name: "3cay", value: `Chơi 3 cây. \`3cay <tiền cược>\`` },
+        { name: 'work', value: 'Làm việc kiếm tiền' },
+        { name: 'with', value: 'Rút tiền từ ngần hàng' },
+        { name: 'dep', value: 'Cất tiền vào ngân hàng' },
+        { name: 'cf', value: `Đá gà. \`cf <tiền cược>\`` },
+        { name: 'give', value: `Chuyển tiền. \`give <người nhận> <số tiền>\`` },
+        { name: 'de', value: `Chơi đề. \`de <tiền cược> <số cược>\`` },
+        { name: 'money', value: 'Kiểm tra số tiền của mình' },
+        { name: 'add-money', value: 'Cộng tiền cho người chơi' },
+        { name: '3cay', value: `Chơi 3 cây. \`3cay <tiền cược>\`` },
+        {
+          name: 'Tạo bàn chơi 3 cây',
+          value: `\`3cay new\` \`3cay join @user\` \`3cay ready\` \`3cay bet <tiền cược>\` \`3cay start\` \`3cay info\``,
+        },
       ],
     };
 
